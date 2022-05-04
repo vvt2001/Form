5bd2c989116983ebaace4f776c2b7ca2af273b2e
vvt2001 <vuvietthang1508@gmail.com>
2022-05-04 23:11:17 +0700

    frame

--- app/src/main/java/com/example/form/MainActivity.java
+++ app/src/main/java/com/example/form/MainActivity.java
@@ -6,44 +6,57 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button confirmButton;
     TextInputLayout mssv;
     TextInputLayout name;
-    EditText cccd;
-    EditText phoneNumber;
-    EditText email;
+    TextInputLayout cccd;
+    TextInputLayout phoneNumber;
+    TextInputLayout email;
     EditText homeTown;
     EditText address;
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+    CheckBox ccpp;
+    CheckBox java;
+    CheckBox python;
+    CheckBox agree;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        confirmButton = (Button) findViewById(R.id.confirmButton);
         mssv = (TextInputLayout) findViewById(R.id.mssv);
         name = (TextInputLayout) findViewById(R.id.name);
-        cccd = (EditText) findViewById(R.id.cccd);
-        phoneNumber = (EditText) findViewById(R.id.phoneNumber);
-        email = (EditText) findViewById(R.id.email);
+        cccd = (TextInputLayout) findViewById(R.id.cccd);
+        phoneNumber = (TextInputLayout) findViewById(R.id.phoneNumber);
+        email = (TextInputLayout) findViewById(R.id.email);
         homeTown = (EditText) findViewById(R.id.homeTown);
         address = (EditText) findViewById(R.id.address);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        ccpp = (CheckBox) findViewById(R.id.ccpp);
+        java = (CheckBox) findViewById(R.id.java);
+        python = (CheckBox) findViewById(R.id.python);
+        agree = (CheckBox) findViewById(R.id.agree);
 
-        confirmButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (TextUtils.isEmpty(mssv.getEditText().getText().toString())){
-                    Toast.makeText(MainActivity.this, "Required fields must be filled", Toast.LENGTH_SHORT ).show();
-                }
-                else{
-                    Toast.makeText(MainActivity.this, "Submitted", Toast.LENGTH_SHORT ).show();
-                }
-            }
-        });
+
+//        confirmButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (TextUtils.isEmpty(mssv.getEditText().getText().toString())){
+//                    Toast.makeText(MainActivity.this, "Required fields must be filled", Toast.LENGTH_SHORT ).show();
+//                }
+//                else{
+//                    Toast.makeText(MainActivity.this, "Submitted", Toast.LENGTH_SHORT ).show();
+//                }
+//            }
+//        });
     }
 
     private boolean confirmMssv(){
@@ -54,6 +67,70 @@ public class MainActivity extends AppCompatActivity {
         } else{
             mssv.setError(null);
             return true;
+        }
+    }
+    private boolean confirmName(){
+        String input = name.getEditText().getText().toString();
+        if (input.isEmpty()){
+            name.setError("Required");
+            return false;
+        } else{
+            name.setError(null);
+            return true;
+        }
+    }
+    private boolean confirmCccd(){
+        String input = cccd.getEditText().getText().toString();
+        if (input.isEmpty()){
+            cccd.setError("Required");
+            return false;
+        } else{
+            cccd.setError(null);
+            return true;
+        }
+    }
+    private boolean confirmPhoneNumber(){
+        String input = phoneNumber.getEditText().getText().toString();
+        if (input.isEmpty()){
+            phoneNumber.setError("Required");
+            return false;
+        } else{
+            phoneNumber.setError(null);
+            return true;
+        }
+    }
+    private boolean confirmEmail(){
+        String input = email.getEditText().getText().toString();
+        if (input.isEmpty()){
+            email.setError("Required");
+            return false;
+        } else{
+            email.setError(null);
+            return true;
+        }
+    }
+    public void confirmInput(View view){
+        if (!confirmMssv() | !confirmName() | !confirmCccd() | !confirmPhoneNumber() | !confirmEmail() | !checked()){
+            return;
+        }
+//        if (radioGroup.getCheckedRadioButtonId() == -1)
+//        {
+//            Toast.makeText(MainActivity.this, "Please select a field of study", Toast.LENGTH_SHORT ).show();
+//        }
+        Toast.makeText(MainActivity.this, "Submitted", Toast.LENGTH_SHORT ).show();
+    }
+    public void chosen(View view){
+        int radioId = radioGroup.getCheckedRadioButtonId();
+        radioButton = findViewById(radioId);
+
+    }
+    boolean checked() {
+        if(agree.isChecked()) {
+            return true;
+        }
+        else {
+            Toast.makeText(MainActivity.this, "Please agree terms & conditions", Toast.LENGTH_SHORT ).show();
+            return false;
         }
     }
 }
